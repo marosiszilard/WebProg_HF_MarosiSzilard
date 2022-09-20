8dbf3b269c587523d3ccd9c0e3894ebe70264266
Marosi Szilard <marosiszily@gmail.com>
2022-09-20 08:47:06 +0300

    elsohazi

---
+++ Hazi1.php
@@ -0,0 +1,58 @@
+<?php
+////1. Feladat
+//$nap = date("l");
+//$datum = date("Y/m/d");
+//echo "A mai datum: $datum";
+//echo "<br>";
+//if ($nap == "Monday") {
+//    echo "A mai nap hetfo";
+//} elseif ($nap == "Tuesday") {
+//    echo "A mai nap kedd";
+//} elseif ($nap == "Wednesday") {
+//    echo "A mai nap szerda";
+//} elseif ($nap == "Thursday") {
+//    echo "A mai nap csutortok";
+//} elseif ($nap == "Friday") {
+//    echo "A mai nap pentek";
+//} elseif ($nap == "Saturday") {
+//    echo "A mai nap szombat";
+//} elseif ($nap == "Sunday") {
+//    echo "A mai nap vasarnap";
+//}
+?>
+
+//2. Feladat
+
+<?php
+//$elso_szam = readline();
+//$masodik_szam = readline();
+//$muvelet = $_POST['muvelet'];
+//$eredmeny = '';
+//if (isset($_POST['submit'])) {
+//    if (is_numeric($elso_szam) && is_numeric($masodik_szam)) {
+//        switch ($muvelet) {
+//            case  "Hozzaad":
+//                $eredmeny = $elso_szam + $masodik_szam;
+//            case "Kivon":
+//                $eredmeny = $elso_szam - $masodik_szam;
+//            case "Szoroz":
+//                $eredmeny = $elso_szam * $masodik_szam;
+//            case "Eloszt":
+//                $eredmeny = $elso_szam / $masodik_szam;
+//        }
+//    }
+//}
+//echo "<h1>{$_POST['elso_szam']} {$_POST['muvelet']} {$_POST['masodik_szam']} equals {$eredmeny}</h1>";
+?>
+<html>
+<!---->
+<!--<input name="elso_szam" type="text" class="form-control"/>-->
+<!--<select name="muvelet">-->
+<!--    <option value="Hozzaad">Hozzaad</option>-->
+<!--    <option value="Kivon">Kivon</option>-->
+<!--    <option value="Szoroz">Szoroz</option>-->
+<!--    <option value="Eloszt">Eloszt</option>-->
+<!--</select>-->
+<!--<input name="number2" type="text" class="form-control"/>-->
+<!--<input name="submit" type="submit" value="Kiszamol">-->
+</html>
