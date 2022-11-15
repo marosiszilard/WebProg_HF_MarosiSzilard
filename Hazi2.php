aa0785dca4e6be52d14d4b2e7f6ae4c4ac648e95
marosiszilard <marosiszily@gmail.com>
2022-11-15 22:10:17 +0200

    Delete Items

---
+++ Hazi2.php
@@ -0,0 +1,10 @@
+<?php
+
+$array1 = ([5, '5', '05', 12.3, '16.7', 'five', 0xDECAFBAD, '10e200']);
+echo gettype($array1);
+foreach ($array1 as $element) {
+    if (is_numeric($element)){
+
+    }
+}
+?>
